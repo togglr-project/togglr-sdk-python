7aa481fbc678d5618fabeed61a2a59c1f3e733c4
Roman Chudov <roman.chudov@gmail.com>
2025-10-18 09:29:04 +0300

    v1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="togglr-sdk",
-    version="1.0.0",
+    version="1.1.0",
     author="Roman Chudov",
     author_email="roman.chudov@gmail.com",
     description="Python SDK for Togglr feature flag management system",
